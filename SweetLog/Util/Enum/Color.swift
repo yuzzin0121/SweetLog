3d07f89ec3deecb221c10c80a370b438d16eb354
yuzzin0121 <ydbwlsjki60@gmail.com>
2024-04-11 01:05:41 +0900

    [Add] 컬러 에셋 추가

---
+++ SweetLog/Util/Enum/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  SweetLog
+//
+//  Created by 조유진 on 4/11/24.
+//
+
+import UIKit
+
+enum Color {
+    static let white: UIColor = .customWhite
+    static let black: UIColor = .customBlack
+    static let gray: UIColor = .customGray
+}

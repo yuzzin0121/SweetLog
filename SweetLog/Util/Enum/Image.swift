6c7ae6437eac9cbfbb7664abcef8da8d6b17c4ee
yuzzin0121 <ydbwlsjki60@gmail.com>
2024-04-11 01:08:12 +0900

    [Add] 이미지 에셋 추가

---
+++ SweetLog/Util/Enum/Image.swift
@@ -0,0 +1,23 @@
+//
+//  Image.swift
+//  SweetLog
+//
+//  Created by 조유진 on 4/11/24.
+//
+
+import UIKit
+
+enum Image {
+    static let arrowLeft: UIImage = .arrowLeft
+
+    static let home: UIImage = .home
+    static let homeFill: UIImage = .homeFill
+    static let map: UIImage = .map
+    static let mapFill: UIImage = .mapFill
+    static let profile: UIImage = .profile
+    static let profileFill: UIImage = .profileFill
+    
+    static let heart: UIImage = .heart
+    static let heartFill: UIImage = .heartFill
+    static let current: UIImage = .currentLocation
+}

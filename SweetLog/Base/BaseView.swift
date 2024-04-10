4dbcc27889950285036a2c43d13fd02e15c6d31b
yuzzin0121 <ydbwlsjki60@gmail.com>
2024-04-11 01:25:46 +0900

    [Feat] BaseView 구현

--- SweetLog/Base/BaseView.swift
+++ SweetLog/Base/BaseView.swift
@@ -5,4 +5,29 @@
 //  Created by 조유진 on 4/11/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class BaseView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = Color.white
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    func configureHierarchy() {
+        
+    }
+    func configureLayout() {
+        
+    }
+    func configureView() {
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

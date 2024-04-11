06042f35bc50d1f6364f2fa47494793265267c5f
yuzzin0121 <ydbwlsjki60@gmail.com>
2024-04-12 00:36:18 +0900

    [Design] 회원가입 화면 디자인

---
+++ SweetLog/CustomView/SignLabel.swift
@@ -0,0 +1,23 @@
+//
+//  SignLabel.swift
+//  SweetLog
+//
+//  Created by 조유진 on 4/12/24.
+//
+
+import UIKit
+
+class SignLabel: UILabel {
+    
+    init(title: String) {
+        super.init(frame: .zero)
+        font = .pretendard(size: 17, weight: .semiBold)
+        text = title
+        textColor = Color.black
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- SweetLog/Scene/SignUpView.swift
+++ SweetLog/Scene/SignUpView.swift
@@ -6,42 +6,97 @@
 //
 
 import Foundation
+import UIKit
 
 final class SignUpView: BaseView {
+    let emailLabel = SignLabel(title: "이메일")
     let emailTextField = SignTextField(placeholderText: "이메일")
+    let emailMessageLabel = UILabel()
+    let duplicateCheckButton = UIButton()
+    
+    let passwordLabel = SignLabel(title: "비밀번호")
     let passwordTextField = SignTextField(placeholderText: "비밀번호")
+    let passwordMessageLabel = UILabel()
+    
+    let nicknameLabel = SignLabel(title: "닉네임")
     let nicknameTextField = SignTextField(placeholderText: "닉네임")
+    let nicknameMessageLabel = UILabel()
     let signUpButton = SignButton(title: "회원가입")
     
     override func configureHierarchy() {
-        addSubviews([emailTextField, passwordTextField, nicknameTextField, signUpButton])
+        addSubviews([emailLabel, emailTextField, duplicateCheckButton, emailMessageLabel,
+                     passwordLabel, passwordTextField, passwordMessageLabel,
+                     nicknameLabel, nicknameTextField, nicknameMessageLabel,
+                     signUpButton])
     }
     override func configureLayout() {
+        emailLabel.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide).offset(150)
+            make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(20)
+            make.height.equalTo(17)
+        }
         emailTextField.snp.makeConstraints { make in
             make.height.equalTo(55)
-            make.top.equalTo(safeAreaLayoutGuide).offset(250)
+            make.top.equalTo(emailLabel.snp.bottom).offset(12)
+            make.leading.equalTo(safeAreaLayoutGuide).inset(20)
+            make.trailing.equalTo(duplicateCheckButton.snp.leading).offset(-16)
+        }
+        duplicateCheckButton.snp.makeConstraints { make in
+            make.top.equalTo(emailTextField)
+            make.trailing.equalTo(safeAreaLayoutGuide).inset(20)
+            make.height.equalTo(55)
+            make.width.equalTo(80)
+        }
+        emailMessageLabel.snp.makeConstraints { make in
+            make.top.equalTo(emailTextField.snp.bottom).offset(8)
             make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(20)
+            make.height.equalTo(14)
         }
         
+        passwordLabel.snp.makeConstraints { make in
+            make.top.equalTo(emailMessageLabel.snp.bottom).offset(30)
+            make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(20)
+            make.height.equalTo(17)
+        }
         passwordTextField.snp.makeConstraints { make in
             make.height.equalTo(55)
-            make.top.equalTo(emailTextField.snp.bottom).offset(30)
+            make.top.equalTo(passwordLabel.snp.bottom).offset(12)
             make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(20)
         }
+        passwordMessageLabel.snp.makeConstraints { make in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(8)
+            make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(20)
+            make.height.equalTo(14)
+        }
         
+        nicknameLabel.snp.makeConstraints { make in
+            make.top.equalTo(passwordMessageLabel.snp.bottom).offset(30)
+            make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(20)
+            make.height.equalTo(17)
+        }
         nicknameTextField.snp.makeConstraints { make in
             make.height.equalTo(55)
-            make.top.equalTo(passwordTextField.snp.bottom).offset(30)
+            make.top.equalTo(nicknameLabel.snp.bottom).offset(12)
             make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(20)
+        }
+        nicknameMessageLabel.snp.makeConstraints { make in
+            make.top.equalTo(nicknameTextField.snp.bottom).offset(8)
+            make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(20)
+            make.height.equalTo(14)
         }
         
         signUpButton.snp.makeConstraints { make in
             make.height.equalTo(55)
-            make.top.equalTo(nicknameTextField.snp.bottom).offset(30)
+            make.top.greaterThanOrEqualTo(nicknameMessageLabel.snp.bottom).offset(30)
             make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(20)
+            make.bottom.equalTo(safeAreaLayoutGuide).inset(12)
         }
     }
     override func configureView() {
-        
+        duplicateCheckButton.backgroundColor = Color.borderGray
+        duplicateCheckButton.setTitle("중복체크", for: .normal)
+        duplicateCheckButton.setTitleColor(Color.black, for: .normal)
+        duplicateCheckButton.titleLabel?.font = .pretendard(size: 15, weight: .semiBold)
+        duplicateCheckButton.layer.cornerRadius = 12
     }
 }

991914b14aa1bc84ff90f5cc04f39b06f50cc4d9
yuzzin0121 <ydbwlsjki60@gmail.com>
2024-05-10 23:52:54 +0900

    [Add] 결제 관련 info.plist 설정

--- SweetLog/Application/AppDelegate.swift
+++ SweetLog/Application/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import IQKeyboardManagerSwift
+import iamport_ios
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -35,7 +36,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+extension AppDelegate {
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        Iamport.shared.receivedURL(url)
+        return true
+    }
+}
